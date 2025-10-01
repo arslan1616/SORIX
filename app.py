96ab9af689d38d4261e086202f7ae2aaff3c7d0b
arslan1616 <114365092+arslan1616@users.noreply.github.com>
2025-10-02 00:19:07 +0300

    Set debug=False and fix Render port issue

--- app.py
+++ app.py
@@ -225,4 +225,4 @@ def download_file(filename):
 if __name__ == '__main__':
    # app.run(debug=True)
    port = int(os.environ.get("PORT", 5000))  # Render portunu al
-   app.run(host="0.0.0.0", port=port)
+   app.run(host="0.0.0.0", port=port, debug=False)
